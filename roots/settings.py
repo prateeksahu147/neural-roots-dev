4ec6272cd3924a9f5e8e86d42d6a439c5bba27b7
Prateek Sahu <prateeksahu147@gmail.com>
2021-05-03 15:11:17 +0530

    database addons  setting.py

--- roots/settings.py
+++ roots/settings.py
@@ -102,12 +102,12 @@ DATABASES = {
     }
 }
 
-"""
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-"""
+
 
 
 # Password validation
